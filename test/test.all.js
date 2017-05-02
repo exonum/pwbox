86f48d977600c74317ec2beeaae9c32685105ced
Alex Ostrovski <slowli@users.noreply.github.com>
2017-05-02 21:50:06 +0300

    Promise polyfill for PhantomJS

--- test/test.all.js
+++ test/test.all.js
@@ -1,3 +1,8 @@
+if (typeof window !== 'undefined' && !window.Promise) {
+  // A (very clumsy) polyfill for PhantomJS
+  window.Promise = require('promise');
+}
+
 require('./test.constants');
 require('./test.serializer');
 require('./test.pwbox');
