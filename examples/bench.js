67411dbac1a923b848b99a7c117933e73f7b7b79
Alex Ostrovski <slowli@users.noreply.github.com>
2017-12-07 14:39:23 +0200

    Fix nitpicks with bench script
    - Make script executable
    - Add copyright notice
    - Add missing message sizes

--- examples/bench.js
+++ examples/bench.js
@@ -1,3 +1,21 @@
+#!/usr/bin/env node
+
+/**
+ * @license
+ * Copyright 2017 The Exonum Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 'use strict';
 
 const bench = require('benchmark');
@@ -6,7 +24,7 @@ const sodiumPwbox = pwbox.withCrypto('libsodium');
 
 // Tested message lengths
 const dataLengths = [
-  32, 64, 128, 256, 512, 1024, 4096, 16384, 65536
+  32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536
 ];
 const password = 'correct horse battery staple';
 // Interval (in seconds) between bench cycles
